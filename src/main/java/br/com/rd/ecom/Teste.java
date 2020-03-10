f0f712d02e9a531c3c8cdbc5aa42a73dc5d3f2fe
Treinamento <Treinamento@RD723894.raiadrogasil.com>
2020-03-10 11:34:37 -0300

    back

---
+++ src/main/java/br/com/rd/ecom/Teste.java
@@ -0,0 +1,10 @@
+package br.com.rd.ecom;
+
+import br.com.rd.ecom.user.model.User;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        User user = new User(2L, "João", "aaa", 18);
+    }
+}
---
+++ src/main/java/br/com/rd/ecom/user/UserController.java
@@ -0,0 +1,22 @@
+package br.com.rd.ecom.user;
+
+import br.com.rd.ecom.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create-user")
+    public User save (@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
